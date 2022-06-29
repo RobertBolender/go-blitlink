98d16bd69d0f7aa5a5c9d4167a754f1f10290584
Robert Bolender <robert@bolender.email>
2022-06-28 22:42:51 -0700

    Add optimistic usage text

--- main.go
+++ main.go
@@ -10,7 +10,40 @@ import (
 
 func main() {
 	if len(os.Args) < 2 {
-		log.Fatal("Usage: go-blitlink <dbfile>")
+		log.Fatal(`
+Usage: go-blitlink <dbfile> <command> [<args>]
+
+Valid commands: insert, query, update, delete
+
+insert
+------
+Arguments: <text> <link> <title> <shortcut>
+Example: go-blitlink mydb.db insert "Hello World" "http://google.com" "Google" "g"
+
+All fields are required, empty strings are permitted
+Example: go-blitlink mydb.db insert "Hello World" "http://google.com" "" "g"
+
+query
+-----
+Arguments: <text>
+Example: go-blitlink mydb.db query "Hello World"
+
+Performs a full-text search across all columns
+Exact matches for the "shortcut" column are listed first
+
+update
+------
+Arguments: <id> <text> <link> <title> <shortcut>
+Example: go-blitlink mydb.db update 1 "Hello World" "http://github.com" "GitHub" "g"
+
+All fields are required, empty strings are permitted
+Example: go-blitlink mydb.db update 1 "Hello World" "http://github.com" "" "g"
+
+delete
+------
+Arguments: <id>
+Example: go-blitlink mydb.db delete 1
+		`)
 	}
 
 	db, err := sql.Open("sqlite3", os.Args[1])
